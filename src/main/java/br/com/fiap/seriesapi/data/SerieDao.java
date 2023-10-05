a742cefefa417ea7298346ae20538874cde63fab
pedroantuness <RM551409@fiap.com.br>
2023-10-05 15:21:20 -0300

    first commit

---
+++ src/main/java/br/com/fiap/seriesapi/data/SerieDao.java
@@ -0,0 +1,38 @@
+package br.com.fiap.seriesapi.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fiap.seriesapi.model.Serie;
+
+public class SerieDao {
+	
+	List<Serie> series = new ArrayList<>();
+	
+	public SerieDao() {
+		var serie1 = new Serie(
+				1L, 
+				"Lost", 
+				"Todos morreram...", 
+				"http...",
+				4.0
+			);
+		var serie2 = new Serie(
+				2L, 
+				"Os outros", 
+				"Todos brigam...", 
+				"http...",
+				5.0
+			);
+		series.add(serie1);
+		series.add(serie2);
+		
+	}
+
+	public List<Serie> findAll(){
+		
+		return series;
+		
+	}
+
+}
---
+++ src/main/java/br/com/fiap/seriesapi/model/Serie.java
@@ -0,0 +1,11 @@
+package br.com.fiap.seriesapi.model;
+
+public record Serie(
+		
+	Long id,
+	String titulo,
+	String sinopse,
+	String poster,
+	Double nota
+		
+) {}
